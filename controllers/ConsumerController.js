8b492bacc3def3a63a7d433dc62ff2e9a4aae704
Yedarm Park <yedarmpark79@gmail.com>
2023-02-04 18:29:07 -0800

    parser updates

--- controllers/ConsumerController.js
+++ controllers/ConsumerController.js
@@ -5,51 +5,48 @@ module.exports = {
 
     get: async (req, res) => {
         const endpoint = req.body.endpoint;
-        let keySets = [];
+        let keys = new Array();
 
         async function consumerPayload() {
             try {
                 const response = await axios.get(endpoint);
 
                 const obj = {
-                    "book": {
-                        "name": "Harry Potter and the Goblet of Fire",
-                        "author": "J. K. Rowling",
-                        "year": 2000,
-                        "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
-                        "genre": "Fantasy Fiction",
-                        "price": {
-                            "paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"
+                    "book":
+                    [
+                        {
+                            "name": "Harry Potter and the Goblet of Fire",
+                            "author": "J. K. Rowling",
+                            "year": 2000,
+                            "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
+                            "genre": "Fantasy Fiction",
+                            "price": {
+                                "paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"
+                            }
+                        },
+                        {
+                            "name": "The Lord of the Rings",
+                            "author": "J. R. R. Tolkien",
+                            "year": 1954,
+                            "characters": ["Gandalf", "Frodo Baggins", "Aragorn"],
+                            "genre": "Fantasy Fiction",
+                            "price": {
+                                "paperback": "$18.45", "hardcover": "$36.50", "kindle": "$7.17"
+                            }
                         }
-                    }
+                    ]
                 };
 
-                function constructKeyHelper(constructKeys) {
-                    let concatKeys = "";
-                    for (let i = 0; i < constructKeys.length; i++) {
-                        concatKeys = concatKeys.concat(constructKeys[i]);
-                        if (!(i+1 === constructKeys.length)) concatKeys = concatKeys.concat(".");
-                    }
-                    return concatKeys;
-                }
-
-                function printKeys(obj, constructKeys) {
+                function parseJson(obj, constructKeys) {
                     for (let k in obj) {
                         constructKeys.push(k);
-                        if (obj[k] instanceof Object) {
-                            if (isNaN(k)) keySets.push(constructKeyHelper(constructKeys));
-                            printKeys(obj[k], constructKeys);
-                        } else {                            
-                            if (isNaN(k)) keySets.push(constructKeyHelper(constructKeys));
-                            constructKeys.pop();
-                        }
+                        keys.push([...constructKeys]);
+                        if (obj[k] instanceof Object) parseJson(obj[k], constructKeys);
+                        else constructKeys.pop();
                     }
                 }
 
-                console.log("--->");
-
-                printKeys(obj, []);
-
+                parseJson(obj, []);
             } catch (error) {
                 console.error(error);
             }
@@ -58,15 +55,42 @@ module.exports = {
         await consumerPayload();
 
         let payload = "{";
-        for (let i = 0; i < keySets.length; i++) {
-            let temp = i + 1;
-            temp = temp.toString();
-            payload = payload + "\"" + "keyset" + temp + "\"" + ":" + "\"" + keySets[i] + "\"";
-            if (!(i+1 === keySets.length)) payload = payload + ",";
-        }
-        payload += "}"
+        let keysToString = "";
+        let keysToStringArr = new Array();
+        for (let i = 0; i < keys.length; i++) {
+            for (let j = 0; j < keys[i].length; j++) {
+                if (isNaN(keys[i][j])) keysToString += keys[i][j] + "."; // // takes care of this scenario: book.characters.0, book.1.author
+            }
 
-        payload = JSON.parse(payload)
+            // takes care of this scenario: book.characters., book.characters.price.paperback.
+            if (keysToString.charAt(keysToString.length - 1) === ".") {
+                keysToString = keysToString.split("");
+                keysToString.pop();
+                keysToString = keysToString.join("");
+            }
+
+            // takes care of duplicates
+            let exist = false;
+            for (let k = 0; k < keysToStringArr.length; k++) {
+                if (keysToStringArr[k].localeCompare(keysToString) === 0) exist = true;
+            }
+
+            if (!exist) {
+                keysToStringArr.push(keysToString);
+
+                payload = payload + "\"" + "keyset" + i.toString() + "\"" + ":";
+                payload += "\"" + keysToString + "\"";
+                if (!(i+1 === keys.length)) payload += ",";
+            }
+
+            keysToString = "";
+        }
+        payload += "}";
+
+        console.log("--->");
+        console.log(payload);
+        
+        payload = JSON.parse(payload);
 
         res.send(payload);
     },
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.urlencoded( { extended: true } ));
 
 app.use("/consumer", consumerRoute);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
